08f847f47474175d44e55f051344e2b3ff17defe
Spencer Cheng <89827794+spencercheng@users.noreply.github.com>
2023-01-12 19:04:25 -0700

    Add project files.

---
+++ Mission 2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mission_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
