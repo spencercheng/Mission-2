c5e0a22a9447885e9438a9c4e21b1d84dd3130b8
Spencer Cheng <89827794+spencercheng@users.noreply.github.com>
2023-01-17 10:05:51 -0700

    Mission 2 Dice roll

---
+++ Mission 2/Dice.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission_2
+{
+    class Dice
+    {
+        public void DiceRolls (int rolls)
+        {
+            int[] randomDiceRolls = new int[11]; // Array of 12 spots
+            Random rnd = new Random(); // Calling Random "package"
+            for (int j = 0; j < rolls; j++) // Generating dice rolls based on what the prompt was 
+            {
+               int dieNum1 = rnd.Next(1, 7);
+               int dieNum2 = rnd.Next(1, 7);
+
+                randomDiceRolls[dieNum1 + dieNum2 - 2]++; //  Storing it into the array
+            }
+            for (int i = 0; i < 11; i++)
+
+            {
+                double percentage = (randomDiceRolls[i] / (double)rolls) * 100; // Calculating the percentage
+
+                Console.Write(i + 2 + ": "); // Printing 2-12
+
+                for (int e = 0; e < percentage; e++) 
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine("");
+            }
+
+        }
+    }
+}
--- Mission 2/Program.cs
+++ Mission 2/Program.cs
@@ -6,7 +6,17 @@ namespace Mission_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dice D = new Dice(); // Instantation
+
+            Console.WriteLine("Welcome to the dice throwing simulator!"); 
+            Console.Write("How many dice rolls would you like to simulate?"); // Prompts
+            int diceRolls = Convert.ToInt32(Console.ReadLine()); // Asking them to enter number
+
+
+            D.DiceRolls(diceRolls); // Calling class
+
+            Console.WriteLine("Thank you for using the dice throwing simulator. Goodbye!");
+
         }
     }
 }
