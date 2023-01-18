1096663ef7121ee95321265f52d8e6ce892faa05
Spencer Cheng <89827794+spencercheng@users.noreply.github.com>
2023-01-18 12:55:32 -0700

    Indent and editing

--- Mission 2/Dice.cs
+++ Mission 2/Dice.cs
@@ -12,17 +12,20 @@ namespace Mission_2
         {
             int[] randomDiceRolls = new int[11]; // Array of 12 spots
             Random rnd = new Random(); // Calling Random "package"
+
             for (int j = 0; j < rolls; j++) // Generating dice rolls based on what the prompt was 
             {
                int dieNum1 = rnd.Next(1, 7);
                int dieNum2 = rnd.Next(1, 7);
 
-                randomDiceRolls[dieNum1 + dieNum2 - 2]++; //  Storing it into the array
+               randomDiceRolls[dieNum1 + dieNum2 - 2]++; //  Storing it into the array
             }
+
             Console.WriteLine(" ");
             Console.WriteLine("DICE ROLLING SIMULATION RESULTS");
             Console.WriteLine("Each '*' represents 1 % of the total number of rolls.");
             Console.WriteLine("Total number of rolls = " + rolls);
+
             for (int i = 0; i < 11; i++)
 
             {
